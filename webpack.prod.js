e6ba00124caf7b676412ae8060edc7d05f74e8df
Adolf Daniel <adolf_daniel@symantec.com>
2018-06-18 22:58:35 -0700

    adding boilerplate

---
+++ webpack.prod.js
@@ -0,0 +1,117 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+/*
+ * We've enabled MiniCssExtractPlugin for you. This allows your app to
+ * use css modules that will be moved into a separate CSS file instead of inside
+ * one of your module entries!
+ *
+ * https://github.com/webpack-contrib/mini-css-extract-plugin
+ *
+ */
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader',
+
+        options: {
+          presets: [
+            'env',
+            {
+              modules: true
+            }
+          ],
+
+          plugins: ['syntax-dynamic-import']
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(scss|css)$/,
+
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.ejs.html'
+    })
+  ],
+
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      minChunks: 1,
+      name: false,
+
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        }
+      }
+    }
+  }
+};
